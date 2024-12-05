28dfa24c91bdd13fd2cf0bd94d23d5078885162d
ZLKCyber <88219725+Zlkcyber@users.noreply.github.com>
2024-12-05 19:24:36 +0800

    add auto complete missions

--- main.js
+++ main.js
@@ -75,6 +75,24 @@ const shareBandwidthForAllTokens = async () => {
     for (let i = 0; i < tokens.length; i++) {
         const token = tokens[i];
         const proxy = proxies[i];
+        try {
+            const response = await checkMissions(token, proxy);
+            if (response && Array.isArray(response.missions)) {
+                const availableMissionIds = response.missions
+                    .filter(mission => mission.status === 'available')
+                    .map(mission => mission.missionId);
+
+                logger('Available Missions:', 'info', availableMissionIds.length);
+                for (const missionId of availableMissionIds) {
+                    logger(`Do and complete mission Id: ${missionId}`, 'info');
+                    const completeMission = await doMissions(missionId, token, proxy)
+
+                    logger(`Mission Id: ${missionId} Complete: ${completeMission.message}`)
+                }
+            }
+        } catch (error) {
+            logger('Error checking missions:', 'error', error);
+        };
 
         try {
             await shareBandwidth(token, proxy);
@@ -83,7 +101,54 @@ const shareBandwidthForAllTokens = async () => {
         }
     }
 };
+const checkMissions = async (token, proxy) => {
+    try {
+        const proxyAgent = new HttpsProxyAgent(proxy);
 
+        const response = await fetch('https://api.openloop.so/missions', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+            agent: proxyAgent,  
+        });
+
+        if (!response.ok) {
+            throw new Error(`Failed to Fetch Missions! Status: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data.data;
+
+    } catch (error) {
+        logger('Error Fetch Missions!', 'error', error.message);
+    }
+};
+const doMissions = async (missionId, token, proxy) => {
+    try {
+        const proxyAgent = new HttpsProxyAgent(proxy);
+
+        const response = await fetch(`https://api.openloop.so/missions/${missionId}/complete`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+            agent: proxyAgent,  
+        });
+
+        if (!response.ok) {
+            throw new Error(`Failed to Complete Missions! Status: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        logger('Error Complete Missions!', 'error', error.message);
+    }
+};
 const main = () => {
     logger(banner, 'debug');
     logger('Starting bandwidth sharing each minute...');
